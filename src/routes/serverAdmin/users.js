17e9402a0659e3bad7aec7ed7ee9196d530d25f4
afrizalsofyan <afrizalafaandy@gmail.com>
2022-08-04 11:53:47 +0700

    fix: update validation

--- src/routes/serverAdmin/users.js
+++ src/routes/serverAdmin/users.js
@@ -3,5 +3,6 @@ const users = require('express').Router();
 const userController = require('../../controllers/UsersController');
 
 users.get('/', userController.getAllUsers);
+users.post('/createUser', userController.createUser);
 
 module.exports = users;
