c76179bdb252efa256019ce53f615286c10180ae
refaldy04 <refaldybanggana@gmail.com>
2022-08-06 18:03:59 +0700

    crud_address_detail

---
+++ src/controllers/addressDetail.js
@@ -0,0 +1,85 @@
+const addressDetailModels = require("../models/addressDetail");
+const response = require("../helpers/standardResponse");
+const upload = require("../helpers/upload").single("picture");
+const errorResponse = require("../helpers/errorResponse");
+const { validationResult } = require("express-validator");
+const addressDetail = require("../routes/serverAdmin/addressDetail");
+
+exports.getAllAddressDetail = async (req, res) => {
+  console.log("a");
+  //   const { limit = 4 } = req.query;
+  const addressDetail = await addressDetailModels.getAllAddressDetail();
+
+  return response(
+    res,
+    "message from standard response: request success",
+    addressDetail
+  );
+};
+
+exports.getAddressDetail = async (req, res) => {
+  const { id } = req.params;
+  const addressDetail = await addressDetailModels.getAddressDetail(id);
+  if (addressDetail.length > 0) {
+    return response(res, "Details user", addressDetail[0]);
+  } else {
+    return response(res, "Not found");
+  }
+};
+
+exports.createAddressDetail = async (req, res) => {
+  const addressDetail = await addressDetailModels.createAddressDetail(req.body);
+  return response(res, "Create detail address success", addressDetail);
+};
+
+exports.updateAddressDetail = async (req, res) => {
+  const { id } = req.params;
+  const addressDetail = await addressDetailModels.updateAddressDetail(
+    id,
+    req.body
+  );
+  return response(res, "Edit address detail success", addressDetail);
+};
+
+exports.deleteAddressDetail = async (req, res) => {
+  const { id } = req.params;
+  const addressDetail = await addressDetailModels.deleteAddressDetail(id);
+  return response(res, "Delete Address Detail success", addressDetail);
+};
+
+//   const validation = validationResult(req);
+//   if (!validation.isEmpty()) {
+//     return response(res, "Error occured", validation.array(), 400);
+//   }
+// chatModels.createChat(req.body, (err, result) => {
+//   if (err) {
+//     return errorResponse(err, res);
+//   } else {
+//     return response(res, "Chat created", result);
+//   }
+// });
+
+exports.editChat = (req, res) => {
+  const { id } = req.params;
+
+  //   const validation = validationResult(req);
+  //   if (!validation.isEmpty()) {
+  //     return response(res, "Error occured", validation.array(), 400);
+  //   }
+
+  chatModels.editChat(id, req.body, (err, result) => {
+    if (err) {
+      console.log(err);
+      return errorResponse(err, res);
+    } else {
+      return response(res, "Edit chat successfully", result);
+    }
+  });
+};
+
+exports.deleteChat = (req, res) => {
+  const { id } = req.params;
+  chatModels.deleteChat(id, (result) => {
+    return response(res, "Profile deleted", result[0]);
+  });
+};
---
+++ src/models/addressDetail.js
@@ -0,0 +1,62 @@
+const prisma = require("../helpers/prisma");
+
+// console.log(db);
+
+exports.getAllAddressDetail = async () => {
+  const addressDetail = await prisma.address_details.findMany();
+  console.log(addressDetail);
+  return addressDetail;
+};
+
+exports.getAddressDetail = async (id) => {
+  id = parseInt(id, 10);
+  const addressDetail = await prisma.address_details.findMany({
+    where: {
+      id,
+    },
+  });
+  return addressDetail;
+};
+
+exports.createAddressDetail = async (data) => {
+  console.log(data);
+  if (data.is_primary === "false") {
+    data.is_primary = false;
+  } else {
+    data.is_primary = true;
+  }
+
+  //   parseInt(data.postal_code);
+  data.postal_code = parseInt(data.postal_code);
+  const addressDetail = await prisma.address_details.create({
+    data,
+  });
+  return addressDetail;
+};
+
+exports.updateAddressDetail = async (id, data) => {
+  id = parseInt(id, 10);
+  if (data.is_primary === "false") {
+    data.is_primary = false;
+  } else {
+    data.is_primary = true;
+  }
+
+  //   parseInt(data.postal_code);
+  data.postal_code = parseInt(data.postal_code);
+
+  const addressDetail = await prisma.address_details.update({
+    where: { id },
+    data,
+  });
+
+  return addressDetail;
+};
+
+exports.deleteAddressDetail = async (id) => {
+  id = parseInt(id, 10);
+  const addressDetail = await prisma.address_details.delete({
+    where: { id },
+  });
+  return addressDetail;
+};
