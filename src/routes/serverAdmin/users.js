dab8202ff0d8d553b48031d3bacb59513952e1bb
afrizalsofyan <afrizalafaandy@gmail.com>
2022-08-06 14:03:45 +0700

    fix: update get all users

--- src/routes/serverAdmin/users.js
+++ src/routes/serverAdmin/users.js
@@ -1,19 +1,21 @@
 const users = require('express').Router();
 
-const userController = require('../../controllers/UsersController');
+const userController = require('../../controllers/user');
 
 const validationMiddleware = require('../../middleware/validation');
 const validationRules = require('../validator');
 
 //customer
-users.get('/', userController.getAllUserCustomers);
-users.post('/', validationRules.createClientValidator, validationMiddleware, userController.createUserCustomer);
-users.patch('/:id', validationRules.createClientValidator, validationMiddleware, userController.updateUserCustomer);
-users.delete('/:id', userController.deleteUser);
+// users.get('/', userController.getAllUserCustomers);
+// users.post('/', validationRules.createClientValidator, validationMiddleware, userController.createUserCustomer);
+// users.patch('/:id', validationRules.createClientValidator, validationMiddleware, userController.updateUserCustomer);
+// users.delete('/:id', userController.deleteUser);
 
-//seller
-users.get('/allSellers', userController.getAllUserSellers);
-//admin
-users.get('/allAdmins', userController.getAllUserAdmins);
+// //seller
+// users.get('/allSellers', userController.getAllUserSellers);
+// //admin
+// users.get('/allAdmins', userController.getAllUserAdmins);
+
+users.get('/', userController.getAllUsers2);
 
 module.exports = users;
