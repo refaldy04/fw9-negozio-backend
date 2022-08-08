0955c9e93d6e1603400d3d27a845906f708715cd
afrizalsofyan <afrizalafaandy@gmail.com>
2022-08-09 00:49:31 +0700

    fix: update remake rev

--- src/routes/serverAdmin/product-review.js
+++ src/routes/serverAdmin/product-review.js
@@ -1,30 +1,30 @@
-const productReview = require("express").Router();
-const productReviewController = require("../../controllers/productReview");
-const { body } = require("express-validator");
-const rules = require("../../middleware/productReviewValidator");
-const validation = require("../../middleware/validation");
+const productReview = require('express').Router();
+const productReviewController = require('../../controllers/productReview');
+const { body } = require('express-validator');
+const rules = require('../../middleware/productReviewValidator');
+const validation = require('../../middleware/validation');
 
-productReview.get("/", productReviewController.getAllProductReview);
-productReview.get("/:id", productReviewController.getProductReview);
+productReview.get('/', productReviewController.getAllProductReview);
+productReview.get('/:id', productReviewController.getProductReview);
 
 // GUYS INI BUAT KASIH RATING BINTANG
 productReview.get(
-  "/average/:id",
+  '/average/:id',
   productReviewController.getProductReviewAverage
 );
 
 productReview.post(
-  "/",
+  '/',
   rules.productReviewValidator,
   validation,
   productReviewController.createProductReview
 );
 productReview.patch(
-  "/:id",
+  '/:id',
   rules.productReviewValidator,
   validation,
   productReviewController.updateProductReview
 );
-productReview.delete("/:id", productReviewController.deleteProductReview);
+productReview.delete('/:id', productReviewController.deleteProductReview);
 
 module.exports = productReview;
