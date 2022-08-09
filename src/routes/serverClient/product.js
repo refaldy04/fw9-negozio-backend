80752ef46bfcde19118367d4f81481fee0d3b39c
refaldy04 <refaldybanggana@gmail.com>
2022-08-09 14:27:32 +0700

    fix code after pull

--- src/routes/serverClient/product.js
+++ src/routes/serverClient/product.js
@@ -1,17 +1,36 @@
-// const productRoute = require('express').Router();
-// const productController = require('../../controllers/product');
-// const authMiddleware = require('../../middleware/auth');
-// const uploudMiddleware = require('../../middleware/uploudMiddelware');
-// const uploudFile = require('../../middleware/uploudPhoto');
-// // const validatorRule = require('../validator');
-// // const validationMiddleware = require('../../middleware/validation');
+const productRoute = require("express").Router();
+const productController = require("../../controllers/product");
+const authMiddleware = require("../../middleware/auth");
+const uploudMiddleware = require("../../middleware/uploudMiddelware");
+const uploudFile = require("../../middleware/uploudPhoto");
+// const validatorRule = require('../validator');
+// const validationMiddleware = require('../../middleware/validation');
 
-// productRoute.post('/', authMiddleware, uploudFile, productController.createProduct );
-// productRoute.patch('/:idProduct', authMiddleware, uploudMiddleware, productController.updateProduct );
-// productRoute.patch('/:idProduct/option/:idOption', authMiddleware, uploudMiddleware,  productController.updateProductOption );
-// productRoute.get('/', authMiddleware, productController.getAllProductsUser );
+productRoute.post(
+  "/",
+  authMiddleware,
+  uploudFile,
+  productController.createProduct
+);
+productRoute.patch(
+  "/:idProduct",
+  authMiddleware,
+  uploudMiddleware,
+  productController.updateProduct
+);
+productRoute.patch(
+  "/:idProduct/option/:idOption",
+  authMiddleware,
+  uploudMiddleware,
+  productController.updateProductOption
+);
+productRoute.get("/", authMiddleware, productController.getAllProductsUser);
 
-// //review
-// productRoute.post('/:idProduct/review',authMiddleware, productController.createReviewProduct);
+//review
+productRoute.post(
+  "/:idProduct/review",
+  authMiddleware,
+  productController.createReviewProduct
+);
 
-// module.exports = productRoute;
+module.exports = productRoute;
